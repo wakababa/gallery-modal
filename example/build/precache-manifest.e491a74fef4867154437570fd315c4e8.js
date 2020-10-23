e4b8509dfabc0a2f81993b8d14b2c72d2ce7efad
wakababa <altgblbl@gmail.com>
2020-10-23 12:58:34 +0300

    build

---
+++ example/build/precache-manifest.e491a74fef4867154437570fd315c4e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24de36648cb2c59f4375d1c6fe2eb9df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3e47936bc77843e76490",
+    "url": "./static/css/main.be601951.chunk.css"
+  },
+  {
+    "revision": "2bd1664ca3843d66289d",
+    "url": "./static/js/2.5e472b5f.chunk.js"
+  },
+  {
+    "revision": "05d5bb7b37cd264eb188b9bb0ea9612c",
+    "url": "./static/js/2.5e472b5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e47936bc77843e76490",
+    "url": "./static/js/main.ae14ed16.chunk.js"
+  },
+  {
+    "revision": "316230b104ce9e1728f9",
+    "url": "./static/js/runtime-main.95372651.js"
+  }
+]);
